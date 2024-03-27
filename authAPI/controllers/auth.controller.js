c6dbdb45282a60dc7b0fc64c40b0e8776a94fab4
Debs5312 <debojyotisaha5@gmail.com>
2024-03-27 16:24:44 +0530

    create sign in api route

--- authAPI/controllers/auth.controller.js
+++ authAPI/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,14 +9,41 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res
-      .status(201)
-      .json({
-        success: true,
-        statusCode: 201,
-        message: "User created successfully!",
-      });
+    res.status(201).json({
+      success: true,
+      statusCode: 201,
+      message: "User created successfully!",
+    });
   } catch (error) {
     next(errorHandler(409, "Duplicate username or email!"));
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser)
+      return next(errorHandler(400, "Email not found!! New to this site?"));
+    const validPassword = await bcryptjs.compareSync(
+      password,
+      validUser.password
+    );
+    if (!validPassword) return next(errorHandler(401, "Invalid Password"));
+    // Creating JWT access token for valid user
+    const accessToken = jwt.sign(
+      { id: validUser._id, username: validUser.username },
+      process.env.JWT_SECRET
+    );
+    // Preparing user details without password.
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("token", accessToken, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(errorHandler(500, "Check DB connection"));
   }
 };
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="bg-slate-200">
       <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
         <Link to="/">
-          <h1 className="font-bold">Auth App</h1>
+          <h1 className="font-bold uppercase">Auth App</h1>
         </Link>
         <ul className="flex gap-4">
           <Link to="/">
